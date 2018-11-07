0e903cb7fd7b438955f5d528e26374c7763ef895
Olena83 <lvelga2007@gmail.com>
2018-11-07 21:50:37 +0200

    Завдання 4()

---
+++ src/main/java/com/lits/HashkodEquals/Class.java
@@ -0,0 +1,32 @@
+package com.lits.HashkodEquals;
+
+import java.util.Objects;
+
+public class Class {
+ String firstName;
+ String lastName;
+String razom(){return firstName+" "+lastName;}
+int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Class aClass = (Class) o;
+        return Objects.equals(firstName, aClass.firstName) &&
+                Objects.equals(lastName, aClass.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    //конструктор для equals
+//    Class(String NewfirstName, String NewlastName)
+//  {
+//      this.setFirstName(NewfirstName);
+//      this.setLastName(NewlastName);}
+
+
+}
