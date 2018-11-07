bff82c71659dd866104862eae019f1ec573004ea
Olena83 <lvelga2007@gmail.com>
2018-11-07 22:00:38 +0200

    IC-2018.2.4 <Администратор@KVELGA Overwrite remote C:\Users\Администратор\IdeaProjects\lesson21 to local

--- src/main/java/com/lits/Food/App1.java
+++
@@ -1,9 +0,0 @@
-package com.lits.Food;
-
-public class App1 {public static void main(String[] args) {
-    Food cake=new Food();
-    System.out.println( cake.getPrice());
-    System.out.println(cake.getQuantity());
-    System.out.println( cake.getWeight());
-}
-}
--- src/main/java/com/lits/Sleep/app3.java
+++
@@ -1,8 +0,0 @@
-package com.lits.Sleep;
-
-public class app3 {public static void main(String[] args) {
-    Person Mukola =new Person();
-     Mukola.sleep();
-     Mukola.sleep(45);
-     Mukola.sleep(4,"години");
-}}
